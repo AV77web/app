e7d26d9dcaa35e9e05aca9ca80997b78dacdb993
AV77web <villari.andrea@libero.it>
2026-05-15 11:42:43 +0200

    Inserire le Categoire del Blog

---
+++ app/app/blog/categorie/page.tsx
@@ -0,0 +1,4 @@
+export default function Categorie() {
+    return <h1>Categorie</h1>
+}
+
---
+++ app/app/blog/page.tsx
@@ -0,0 +1,5 @@
+// app/blog/page.js
+export default function Blog(){
+    return <h1>Blog Main Page</h1>;
+    
+}
