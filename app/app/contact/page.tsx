c0e2eaf0bba70651a1302dad3aa7ed19e085623c
AV77web <villari.andrea@libero.it>
2026-05-15 12:04:42 +0200

    inserimento componente Link

--- app/app/contact/page.tsx
+++ app/app/contact/page.tsx
@@ -1,6 +1,8 @@
+import Link from "next/link";
+
 export default function Contact() {
     return <>
         <h1>Contatti</h1>
-        <a href="/about">About</a>
+        <Link href="/about">About</Link>
     </>
 }
