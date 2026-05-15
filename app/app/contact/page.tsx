c52f282929968ac1adba2da1894b7279a4cffcf7
AV77web <villari.andrea@libero.it>
2026-05-15 13:00:34 +0200

    Inseriti componenti Link e Image

--- app/app/contact/page.tsx
+++ app/app/contact/page.tsx
@@ -1,8 +1,10 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Contact() {
     return <>
         <h1>Contatti</h1>
         <Link href="/about">About</Link>
+        <Image src="/globe.svg" alt="Immagine globo"></Image>
     </>
 }
