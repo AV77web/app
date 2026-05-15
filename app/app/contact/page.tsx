f150ec315c6b1502b5aa742c22d1ca487e126509
AV77web <villari.andrea@libero.it>
2026-05-15 13:03:26 +0200

    inserirti attributi componente Image

--- app/app/contact/page.tsx
+++ app/app/contact/page.tsx
@@ -5,6 +5,12 @@ export default function Contact() {
     return <>
         <h1>Contatti</h1>
         <Link href="/about">About</Link>
-        <Image src="/globe.svg" alt="Immagine globo"></Image>
+        <Image
+            src="/globe.svg"
+            alt="Immagine globo"
+            width={800}
+            height={800}
+            sizes="{max-width: 7690px} 100vw, 50vw"></Image>
+
     </>
 }
