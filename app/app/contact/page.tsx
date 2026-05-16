72e1a2b5fcd5489bcbfe621fed4aed7796f5b7fb
AV77web <villari.andrea@libero.it>
2026-05-16 17:06:34 +0200

    Inserita la Navbar nel rootLayout.tsx

--- app/app/contact/page.tsx
+++ app/app/contact/page.tsx
@@ -1,13 +1,11 @@
-import Navbar from "../../components/Navbar";
- 
+
 export default function Contact() {
     return ( <main className="max-w-4xl ma-auto p-8">
 
         <h1 className="text-lg font-bold mb-6">Contattici</h1>
         <div className="text-lg text-gray-600 mb-8">
-            <p>Compila il forma sottostante per metterti in contatto con noi.</p>
+            <p>Compila il form sottostante per metterti in contatto con noi.</p>
         </div>
-        <Navbar/>
 
     </main>)
 }
