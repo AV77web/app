fe548e020c6a4542434661a888ad0423f29115d3
AV77web <villari.andrea@libero.it>
2026-05-18 09:43:31 +0200

    correzione pagina contatti

--- app/app/contact/page.tsx
+++ app/app/contact/page.tsx
@@ -1,8 +1,8 @@
 
 export default function Contact() {
-    return ( <main className="max-w-4xl ma-auto p-8">
+    return ( <main className="max-w-4xl mx-auto p-8">
 
-        <h1 className="text-lg font-bold mb-6">Contattici</h1>
+        <h1 className="text-4xl font-bld mb-6">Contattici</h1>
         <div className="text-lg text-gray-600 mb-8">
             <p>Compila il form sottostante per metterti in contatto con noi.</p>
         </div>
---
+++ app/components/ContactForm.tsx
@@ -0,0 +1,7 @@
+//====================================
+// File: ContactForm.tsx
+// modulo contatti 
+// @author: "villari.andrea@libero.it"
+// @version: "1.0.0 2026-05-18"
+//====================================
+
