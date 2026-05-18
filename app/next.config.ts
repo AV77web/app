3b3b324ef90c11ece9ebf54aec098bd68e11ee7c
AV77web <villari.andrea@libero.it>
2026-05-18 15:35:39 +0200

    correzione 2 a componente Blog

--- app/next.config.ts
+++ app/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
   images: {
-    domains: ['andreavillari.it'],    // con questa autorizzo tutte 
+    /*domains: ['andreavillari.it'],    // con questa autorizzo tutte 
     remotePatterns: [                 // le immagine provenienti da quel 
       {                               // dominio
         protocol: 'https',            // 
@@ -12,7 +12,7 @@ const nextConfig: NextConfig = {
         pathname: '/images/**',
       },
     ],
-    deviceSizes: [ 640, 750, 828, 1080, 1280], // breackpoint responsive
+    deviceSizes: [ 640, 750, 828, 1080, 1280], // breackpoint responsive */
   }
 };
 
